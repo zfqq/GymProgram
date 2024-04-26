ccdee8ef630c12819e28200d646c36bef592ab3f
roam <1193983609@qq.com>
2024-04-26 14:40:49 +0800

    测试提交

--- SupermarketManagerVue/vue.config.js
+++ SupermarketManagerVue/vue.config.js
@@ -12,6 +12,7 @@ module.exports={
   publicPath: process.env.NODE_ENV === 'production'?'/dist/':'/',
     devServer:{
     // host:"59.110.94.218",
+     // host:"59.110.94.218",
     host:"localhost",
     port: 9292
   },
